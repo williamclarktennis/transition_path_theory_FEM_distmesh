3f6fa0604fa8731f18a9b57a2ed169e5646882cb
williamclarktennis <williamclarktennis@gmail.com>
2022-06-30 20:57:09 -0400

    Update 6/30/2022

--- FEM_TPT.py
+++ FEM_TPT.py
@@ -7,11 +7,13 @@
 # grad * (exp(-beta V(x,y) grad q(x,y)) = 0, q(bdry A) = 0, q(bdry B) = 1
 # the potential used is the Face potential defined in this cell
 
+from matplotlib.cbook import pts_to_midstep
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 import csv 
 import scipy
+import scipy.linalg
 from scipy.sparse import csr_matrix
 
 
@@ -115,7 +117,84 @@ def reactive_current_and_transition_rate(pts,tri,fpot,beta,q):
     tcount = np.zeros((Npts,1)) # the number of triangles adjacent to each vertex
     for j in range(Ntri):
         indt = np.array(tri[j,:])[:,None]    
-        Rcurrent_verts[indt,:] = Rcurrent_verts[indt,:] + Rcurrent[j,:]
-        tcount[indt] = tcount[indt] + 1
-    Rcurrent_verts = Rcurrent_verts/np.concatenate((tcount,tcount),axis = 1)
+        Rcurrent_verts[indt,:] = Rcurrent_verts[indt,:] + Rcurrent[j,:] # adds reactive current taken from center of each adjacent triangle to each vertice of the triangle
+        tcount[indt] = tcount[indt] + 1   # updates the rows of tcount with indeces matching the entries in the indt array
+    Rcurrent_verts = Rcurrent_verts/np.concatenate((tcount,tcount),axis = 1) # dividing to make it an average of neighboring triangles
     return Rcurrent_verts, Rrate
+
+def trajectory_probability_density(pts,tri,fpot,beta,q):
+    Npts = np.size(pts,axis=0)
+    Ntri = np.size(tri,axis=0)
+
+    mu = np.zeros((Ntri,1))
+    for j in range(Ntri):
+        ind = tri[j,:]
+        verts = pts[ind,:]
+        vmid = np.reshape(np.sum(verts,axis=0)/3,(1,2))
+        mu[j,:] = np.exp(-beta *fpot(vmid))
+
+    # map reactive current on vertices
+    mu_verts = np.zeros((Npts,1))
+    tcount = np.zeros((Npts,1)) # the number of triangles adjacent to each vertex
+    for j in range(Ntri):
+        indt = np.array(tri[j,:])[:,None]    
+        mu_verts[indt] = mu_verts[indt] + mu[j] # adds mu taken from center of each triangle to each vertice of the triangle
+        tcount[indt] = tcount[indt] + 1   # adds a tri. to the count
+    mu_verts = mu_verts/tcount # dividing to make it an average of neighboring triangles
+    return mu_verts
+
+
+# # parameters for the face potential
+# xa=-3 
+# ya=3
+# xb=0 
+# yb=4.5
+# par = np.array([xa,ya,xb,yb]) # centers of sets A and B
+
+# # problem setup: choose sets A and B and the outer boundary
+# # set A is the circle with center at (xa,ya) and radius ra
+# # set B is the circle with center at (xb,yb) and radius rb
+# ra = 0.5 # radius of set A
+# rb = 0.5 # radius of set B
+# beta = 3 # beta = 1/(k_B T), T = temperature, k_B = Boltzmann's constant
+# Vbdry = 12 # level set of the outer boundary {x : fpot(x) = Vbdry}
+
+# # if generate_mesh = True, mesh is generated and saves as csv files
+# # if generate_mesh = False, mesh is downloaded from those csv files
+# generate_mesh = False
+
+# # h0 is the desired scalind parameter for the mesh
+# h0 = 0.1
+
+
+# def face(xy,par):
+#     xa=par[0]
+#     ya=par[1]
+#     xb=par[2] 
+#     yb=par[3]
+#     x = xy[:,0]
+#     y = xy[:,1]
+#     f=(1-x)**2+(y-0.25*x**2)**2+1
+#     g1=1-np.exp(-0.125*((x-xa)**2+(y-ya)**2))
+#     g2=1-np.exp(-0.25*(((x-xb)**2+(y-yb)**2)))
+#     g3=1.2-np.exp(-2*((x+0)**2+(y-2)**2))
+#     g4=1+np.exp(-2*(x+1.5)**2-(y-3.5)**2-(x+1)*(y-3.5))
+#     v = f*g1*g2*g3*g4
+#     return v
+
+
+# def main():
+    # pts = np.loadtxt('face_pts.csv', delimiter=',', dtype=float)
+    # tri = np.loadtxt('face_tri.csv', delimiter=',', dtype=int)
+    # def fpot(pts):
+        # return face(pts,par)
+    # find the mesh points lying on the Dirichlet boundary \partial A \cup \partial B
+    # NAind,Aind = find_ABbdry_pts(pts,xa,ya,ra,h0) # find mesh points on \partial A
+    # NBind,Bind = find_ABbdry_pts(pts,xb,yb,rb,h0) # find mesh points on \partial B
+
+    # q = FEM_committor_solver(pts,tri,Aind,Bind,fpot,beta)
+
+    # trajectory_probability_density(pts, tri, fpot, beta, q)
+
+# if __name__ == "__main__":
+    # main()
